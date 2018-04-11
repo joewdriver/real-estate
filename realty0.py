329fbde540acd8976dc29949b4785ec55f071e29
Judge Lee <jblee@hmc.edu>
2018-04-10 20:35:52 -0400

    Working NN with output file on April 10, 2018. Need cleaned input files for better tests

--- realty0.py
+++ realty0.py
@@ -11,15 +11,18 @@ from neupy import algorithms, layers
 from neupy import plots
 from neupy.estimators import rmsle
 
+# Import data from file
 data = pd.read_csv('property-assessment-fy2015.csv')
 
 
 # TODO: Need to filter all data
 
+# Values to predict on
 locations = data['Location']
 
+# Prices
 bldg_price = data[['AV_BLDG']].copy()
-land_price = data[['AV_LAND']].copy() # TODO REMOVE
+land_price = data[['AV_LAND']].copy()
 total_price = data[['AV_TOTAL']].copy()
 
 # TODO:Add LU
@@ -32,40 +35,49 @@ for i in range(len(locations)):
     # TODO: TEMP FIX, no lat/lon.
     if type(locations[i]) == type(0.0) or locations[i] == "0":
         a,b = locations[prev].split("|")
-        lat.append(a[1:])
-        lon.append(b[:-1])
+        lat.append(float(a[1:]))
+        lon.append(float(b[:-1]))
     else:
         a,b = locations[i].split("|")
-        lat.append(a[1:])
-        lon.append(b[:-1])
+        lat.append(float(a[1:]))
+        lon.append(float(b[:-1]))
         prev = i
 
 lat = pd.Series(lat,name="latitude")
 lon = pd.Series(lon,name="longitude")
 
-data = pd.concat([lat,lon,bldg_price,land_price,total_price], axis = 1)
+# Only the terms used to predict value
+data = pd.concat([lat,lon], axis = 1)
+
+# Target values. Currently bldg_price, land_price, and total_price
+target = pd.concat([bldg_price,land_price,total_price],axis=1)
 
 
 data_scaler = preprocessing.MinMaxScaler()
 target_scaler = preprocessing.MinMaxScaler()
 
 
-# TODO: Check after preprocessing data. Running into NaN or infinities here. Likely from missing values
 data = data_scaler.fit_transform(data.values)
-target = target_scaler.fit_transform(data.reshape(-1, 1))
+target = target_scaler.fit_transform(target.values)
 
+# Setting seed for reproducibility
 environment.reproducible()
 
 # # split data into training and validation
 x_train, x_test, y_train, y_test = train_test_split(
-    data, data, train_size=0.85
+    data, target, train_size=0.85
 )
 
+# Creating the neural network
+#   connection:
+#       Values being trained on. Currently only lat and lon (2)
+#       Size of hidden layer. Currently arbitrarily set to 50
+#       Size of output values. Currently bldg_price, land_price, and total_price (3)
 cgnet = algorithms.ConjugateGradient(
     connection=[
-        layers.Input(5),
+        layers.Input(2),
         layers.Sigmoid(50),
-        layers.Sigmoid(5),
+        layers.Sigmoid(3),
     ],
     search_method='golden',
     show_epoch=25,
@@ -73,12 +85,25 @@ cgnet = algorithms.ConjugateGradient(
     addons=[algorithms.LinearSearch],
 )
 
+# Train neural net
 cgnet.train(x_train, y_train, x_test, y_test, epochs=100)
 
-# plots.error_plot(cgnet)
-print(x_test)
+
+# Make predictions
 print("Starting predictions")
-y_predict = cgnet.predict(x_test).round(1)
+y_predict = cgnet.predict(x_test)
 error = rmsle(target_scaler.inverse_transform(y_test),
-              target_scaler.inverse_transform(y_predict))
-print(y_predict)
+    target_scaler.inverse_transform(y_predict))
+print(error)
+
+# write values to csv
+#   Row 1: Test cases (lat and lon)
+#   Row 2: Predicted values (prices)
+#   Row 3: Test values (prices)
+#   Row 4: Error
+with open('test_180410.csv','w') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(data_scaler.inverse_transform(x_test))
+    wr.writerow(target_scaler.inverse_transform(y_predict))
+    wr.writerow(target_scaler.inverse_transform(y_test))
+    wr.writerow([error])
