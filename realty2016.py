07a5af7e692c4f80e5a27c1bd752d9f6c49705c6
Sylvia Niu <silnue0801@gmail.com>
2018-04-19 20:33:55 -0400

    extracting lats and lons for each year

--- realty2016.py
+++ realty2016.py
@@ -1,11 +1,5 @@
 import pandas as pd
-from geopy.geocoders import Nominatim
-geolocator = Nominatim()
 
 data = pd.read_csv('property-assessment-fy2016.csv')
-
-st_num = data['ST_NUM']
-st_name = data['ST_NAME']
-st_name_suf = data['ST_NAME_SUF']
-
-data['ST_ADDR'] = pd.Series(data['ST_NUM']).str.cat([data['ST_NAME'], data['ST_NAME_SUF']], sep=' ')
+lats = data['LATITUDE']
+lons = data['LONGITUDE']
---
+++ realty2018.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from geopy.geocoders import Nominatim
+geolocator = Nominatim()
+
+data = pd.read_csv('ast2018full.csv')
+data.ZIPCODE = data.ZIPCODE.str.replace('_', '')
+
+st_num = data['ST_NUM']
+st_name = data['ST_NAME']
+st_name_suf = data['ST_NAME_SUF']
+zip_code = data['ZIPCODE']
+
+data['ST_ADDR'] = pd.Series(st_num).str.cat([st_name, st_name_suf], sep=' ')
+st_addr = data['ST_ADDR']
+data['ST_ADDR'] = pd.Series(st_addr).str.cat([zip_code], sep=' ')
+lats = []
+lons = []
+for i in range(len(data['ST_ADDR'])):
+    temp = geolocator.geocode(data['ST_ADDR'][i])
+    if temp != None:
+        lats.append(temp.latitude)
+        lons.append(temp.longitude)
+data['LATITUDE'] = lats
+data['LONGITUDE'] = lons
