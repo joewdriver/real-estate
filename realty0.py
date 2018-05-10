726321e13df12ef3b7433e4e3e007fb68b48f224
Judge Lee <jblee@hmc.edu>
2018-05-09 20:29:35 -0400

    Now predicting 2018 prices. Achieves rmsle 1.158. Adding 2018 dataset and output file.

--- realty0.py
+++ realty0.py
@@ -16,6 +16,7 @@ from neupy.estimators import rmsle
 data = pd.read_csv('data2015.csv')
 data.append(pd.read_csv('data2013.csv'),ignore_index=True)
 data.append(pd.read_csv('data2011.csv'),ignore_index=True)
+test = pd.read_csv('data2018.csv')
 
 lat = data["latitude"]
 lon = data["longitude"]
@@ -43,16 +44,43 @@ built.astype('float')
 bldg.astype('float')
 land.astype('float')
 
+lat_test = test["latitude"]
+lon_test = test["longitude"]
+year_test = test["year"]
+bdrms_test = test["bedrooms"]
+fbath_test = test["full_bth"]
+hbath_test = test["half_bth"]
+sf_test = test["square_foot"]
+res_test = test["res"]
+condo_test = test["condo"]
+built_test = test["yr_built"]
+bldg_test = test["bldg_price"]
+land_test = test["land_price"]
+
+lat_test.astype('float')
+lon_test.astype('float')
+year_test.astype('float')
+bdrms_test.astype('float')
+fbath_test.astype('float')
+hbath_test.astype('float')
+sf_test.astype('float')
+res_test.astype('float')
+condo_test.astype('float')
+built_test.astype('float')
+bldg_test.astype('float')
+land_test.astype('float')
 
 
 
 # Only the terms used to predict value
 data = pd.concat([lat,lon,year,bdrms,fbath,hbath,sf,res,condo,built], axis = 1)
+test = pd.concat([lat_test,lon_test,year_test,bdrms_test,fbath_test,hbath_test,sf_test,res_test,condo_test,built_test], axis = 1)
 
 
 
 # Target values. Currently bldg_price, land_price
 target = pd.concat([bldg,land],axis=1)
+target_test = pd.concat([bldg_test,land_test],axis=1)
 
 # Normalize data
 data_scaler = preprocessing.MinMaxScaler()
@@ -60,6 +88,9 @@ target_scaler = preprocessing.MinMaxScaler()
 
 data = data_scaler.fit_transform(data.values)
 target = target_scaler.fit_transform(target.values)
+
+test = data_scaler.fit_transform(test.values)
+# target_test = target_scaler.fit_transform(target_test.values)
 
 # Setting seed for reproducibility
 environment.reproducible()
@@ -93,20 +124,25 @@ cgnet.train(x_train, y_train, x_test, y_test, epochs=100)
 
 # Make predictions
 print("Starting predictions")
-y_predict = cgnet.predict(x_test)
-error = rmsle(target_scaler.inverse_transform(y_test),
+y_predict = cgnet.predict(test)
+error = rmsle(target_test,
     target_scaler.inverse_transform(y_predict))
 print(error)
+
+
+# write values to csv
+#   lat,lon,year,bdrms,fbath,hbath,sf,res,condo,built
+with open('test.csv','w') as myfile:
+    wr = csv.writer(myfile,quoting=csv.QUOTE_ALL)
+    wr.writerow(["latitude", "longitude", "year", "bedrooms", "full_bth", "half_bth", "square_foot", "res", "condo", "yr_built", "bldg_price", "land_price"])
+    for i in range(len(y_predict)):
+        wr.writerow(data_scaler.inverse_transform(test)[i].tolist() + target_scaler.inverse_transform(y_predict)[i].tolist())
 
 # write values to csv
 #   Row 1: Test cases (lat and lon)
 #   Row 2: Predicted values (prices)
 #   Row 3: Test values (prices)
 #   Row 4: Error
-with open('test.csv','w') as myfile:
-    wr = csv.writer(myfile,quoting=csv.QUOTE_ALL)
-    for i in range(len(y_predict)):
-        wr.writerow(data_scaler.inverse_transform(x_test)[i].tolist() + target_scaler.inverse_transform(y_predict)[i].tolist())
 # with open('predict_180501.csv','w') as myfile:
 #     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 #     wr.writerow(data_scaler.inverse_transform(x_test))
